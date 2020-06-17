5561edab13f805626ecc89d6dfd7fc2df5ab67db
muhyasin89 <muhyasin89@gmail.com>
2020-06-17 11:15:23 +0700

    [INIT]first input

--- django_form_wizard_lab/student/models.py
+++ django_form_wizard_lab/student/models.py
@@ -2,3 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from model_utils.models import TimeStampedModel
+
+
+class Student(TimeStampedModel):
+    pass
--- django_form_wizard_lab/subject/models.py
+++ django_form_wizard_lab/subject/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from autoslug import AutoSlugField
+
 from model_utils.models import TimeStampedModel
+
+class Subject(TimeStampedModel):
+    name =  models.CharField(_("Subject"), max_length=255)
+    slug = AutoSlugField(populate_from='name')
+    number_sks = models.IntegerField()
